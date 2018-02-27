68db23e5a44266a477bcd3eb6bfc7dbf1f66f406
romainbessugesmeusy <romainbessuges@gmail.com>
2018-02-27 19:03:39 +0100

    Add to index !

--- data/index.js
+++ data/index.js
@@ -1,4 +1,10 @@
 module.exports = {
-    en: {personalDataTypes: require('./en/personalDataTypes')},
-    fr: {personalDataTypes: require('./fr/personalDataTypes')},
+    en: {
+        personalDataTypes: require('./en/personalDataTypes'),
+        countries: require('./en/countries')
+    },
+    fr: {
+        personalDataTypes: require('./fr/personalDataTypes'),
+        countries: require('./fr/countries')
+    },
 };
